0493122f2c16e4c80e4fa447f74083e241c1cf92
Alan Nguyen <nguyenalan@berkeley.edu>
2019-11-03 10:19:12 -0800

    stripped away more code => proper skeleton in visualizer

--- src/visualizer.js
+++ src/visualizer.js
@@ -21,11 +21,15 @@ function initMesh() {
   /* YOUR CODE HERE */
 }
 
+function initAudio(audio) {
+  /* YOUR CODE HERE */
+}
+
 export function init() {
   teardown = false;
   listener = new THREE.AudioListener();
+  scene = new THREE.Scene();
   initRenderer();
-  initScene();
   initCamera();
   initLight();
   initPlane();
@@ -37,87 +41,23 @@ export function init() {
 }
 
 function initRenderer() {
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function initScene() {
-  scene = new THREE.Scene();
-  scene.fog = new THREE.Fog(0x000000, 1, 1000);
+  /* YOUR CODE HERE */
 }
 
 function initGroup() {
-  scene.add(plane);
-  scene.add(plane2);
-  scene.add(camera);
+  /* YOUR CODE HERE */
 }
 
 function initLight() {
-  scene.add(new THREE.AmbientLight(0x222222));
-  light = new THREE.DirectionalLight(0x590d82, 0.5);
-  light.position.set(200, 300, 400);
-  scene.add(light);
-
-  light2 = light.clone();
-  light2.position.set(-200, 300, 400);
-  scene.add(light2);
-
-  var pointLight = new THREE.PointLight(0xffffff);
-  pointLight.position.set(0, 0, 0);
-  scene.add(pointLight);
+  /* YOUR CODE HERE */
 }
 
 function initPlane() {
-  planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
-  planeMaterial = new THREE.MeshLambertMaterial({
-    side: THREE.DoubleSide,
-    wireframe: true
-  });
-
-  plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.rotation.x = -0.5 * Math.PI;
-  plane.position.set(0, 40, 0);
-
-  plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane2.rotation.x = -0.5 * Math.PI;
-  plane2.position.set(0, -40, 0);
+  /* YOUR CODE HERE */
 }
 
 export function render() {
-  if (!teardown) {
-    requestAnimationFrame(render);
-  }
-  // console.log(mesh);
-  // analysis - make vertices spike?
-  data = analyser.getAverageFrequency();
-
-  // standard update
-  mesh.material.uniforms["time"].value += 0.00001;
-
-  // differentiate frequencies. needs more fine tuning (only accurate for more "hard-hitting" songs).
-  // project extension: instead of average frequency, separate by bass, treble, etc.!
-  console.log(data);
-  if (data === 0) {
-    mesh.material.uniforms["freq"].value = 0.1;
-    mesh.material.uniforms["opacity"].value = 0.1;
-  } else if (data < 30) {
-    mesh.material.uniforms["opacity"].value = 0.1;
-    mesh.material.uniforms["freq"].value = data / 2500;
-  } else if (data < 60) {
-    mesh.material.uniforms["opacity"].value = data / 5000;
-    mesh.material.uniforms["freq"].value = data / 1500;
-  } else if (data < 90) {
-    mesh.material.uniforms["opacity"].value = data / 1700;
-    mesh.material.uniforms["freq"].value = data / 300;
-  } else {
-    mesh.material.uniforms["opacity"].value = data / 600;
-    mesh.material.uniforms["freq"].value = data / 25;
-  }
-  controls.update();
-  renderer.render(scene, camera);
+  /* YOUR CODE HERE */
 }
 
 export function setAudio(audioNode) {
@@ -125,22 +65,14 @@ export function setAudio(audioNode) {
   initAudio(audio);
 }
 
-function initAudio(audio) {
-  audioLoader = new THREE.AudioLoader();
-  sound = new THREE.Audio(listener);
-  audioLoader.load(audio.src, function(buffer) {
-    sound.setBuffer(buffer);
-    sound.play();
-  });
-  analyser = new THREE.AudioAnalyser(sound, 2048);
-}
-
+/* Resizes the camera and renderer along with browser width + height changes. */
 function resize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+/* Stops rendering the canvas. */
 export function reset() {
   teardown = true;
 }
